b43c0b05a881549fbb0bee1bbd16141105c9101b
Mustafa <johan0373@gmail.com>
2023-05-09 08:16:37 +0200

    hello

---
+++ src/pages/Home.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import Map from "../Map";
+import { Paper } from "@mui/material";
+import PlacesAutoComplete from "../components/PlacesAutoComplete";
+
+export const Home = () => {
+  const [location, setLocation] = useState(null);
+  const [userRefusal, setUserRefusal] = useState();
+  const [query, setQuery] = useState("");
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    // window.ReactNativeWebView.postMessage("hello world from directions api");
+  });
+
+  useEffect(() => {
+    async function fetchLocations() {
+      if (query === "") return setPlaces([]);
+      const { data } = await axios.get(
+        `https://nominatim.openstreetmap.org/search?q=${query}&format=json&poly_geojson=0&addressdetails=1`
+      );
+
+      console.log(data);
+      setPlaces(data);
+    }
+
+    fetchLocations();
+  }, [query]);
+  return (
+    <div>
+      <Paper>
+        <PlacesAutoComplete />
+      </Paper>
+      <Map
+        location={location}
+        setLocation={setLocation}
+        setUserRefusal={setUserRefusal}
+      />
+    </div>
+  );
+};
---
+++ src/pages/Provider.js
@@ -0,0 +1,41 @@
+import { CircularProgress } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+
+const Provider = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      (error) => console.log("you must allow location to continue"),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+  return (
+    <div>
+      {location ? (
+        <MapContainer
+          center={location}
+          zoom={13}
+          scrollWheelZoom
+          className="w-full min-h-screen">
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker draggable={true} position={location} />
+        </MapContainer>
+      ) : (
+        <CircularProgress />
+      )}
+    </div>
+  );
+};
+
+export default Provider;
